95cfb245ad063127613fcb8533d3b8f9d8ef3483
Илья Артёменко <mov.inc.xor@gmail.com>
2020-05-31 23:27:22 +0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -9,9 +9,9 @@ import API.API
 import API.LongPoll
 
 vk = VK {
-    groupId = "195877323"
-  , accessToken = "f022db701e9d1932231e2722d245778ce9bf483ad179beccc7545d4c1b956e6df632f7b264f2f7a2bcef9"
-  , version = "5.107"
+    groupId = ""
+  , accessToken = ""
+  , version = ""
 }
 
 handler = do
